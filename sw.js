838d814b96af172b05dd18f75eb10061737819c4
surya-impex-app <coolbabyharsh@gmail.com>
2026-09-22 13:57:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 const CACHE='surya-impex-v1';
-const ASSETS=['./','./Index.html','./manifest.json'];
+const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
